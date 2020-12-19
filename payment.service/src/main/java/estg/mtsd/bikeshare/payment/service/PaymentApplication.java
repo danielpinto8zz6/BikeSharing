ad3e7b4484b1365c22b3b0e18d8699e1a15146a5
Daniel Pinto <danielpinto8zz6@gmail.com>
2020-12-19 18:09:18 +0000

    Add architecture and bpmn diagrams

--- payment.service/src/main/java/estg/mtsd/bikeshare/payment/service/PaymentApplication.java
+++ payment.service/src/main/java/estg/mtsd/bikeshare/payment/service/PaymentApplication.java
@@ -1,4 +1,4 @@
-package estg.mtsd.bikeshare.payment;
+package estg.mtsd.bikeshare.payment.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- payment.service/src/test/java/estg/mtsd/bikeshare/payment/service/PaymentApplicationTests.java
+++ payment.service/src/test/java/estg/mtsd/bikeshare/payment/service/PaymentApplicationTests.java
@@ -1,4 +1,4 @@
-package estg.mtsd.bikeshare.payment;
+package estg.mtsd.bikeshare.payment.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
