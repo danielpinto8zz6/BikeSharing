95fc413d87dca4b445bc71f8fb6a952bc005d19d
Daniel Pinto <danielpinto8zz6@gmail.com>
2021-01-17 18:33:15 +0000

    fix notification service

--- notifications.service/src/main/java/estg/mtsd/bikeshare/notifications/service/controllers/NotificationController.java
+++ notifications.service/src/main/java/estg/mtsd/bikeshare/notifications/service/controllers/NotificationController.java
@@ -1,12 +1,15 @@
 package estg.mtsd.bikeshare.notifications.service.controllers;
 
 import com.google.firebase.messaging.FirebaseMessagingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import estg.mtsd.bikeshare.notifications.service.service.FirebaseMessagingService;
+import estg.mtsd.bikeshare.notifications.service.service.TokenService;
 import estg.mtsd.bikeshare.shared.library.vo.NotificationVo;
 
 @Controller
@@ -14,13 +17,18 @@ public class NotificationController {
 
     private final FirebaseMessagingService firebaseService;
 
+    @Autowired
+    TokenService tokenService;
+    
     public NotificationController(FirebaseMessagingService firebaseService) {
         this.firebaseService = firebaseService;
     }
 
     @RequestMapping("/send-notification")
     @ResponseBody
-    public String sendNotification(@RequestBody NotificationVo notification) throws FirebaseMessagingException {
-        return firebaseService.sendNotification(notification);
+    public String sendNotification(@RequestBody NotificationVo notificationVo) throws FirebaseMessagingException {
+        String token = tokenService.findById(notificationVo.getEmail());
+
+        return firebaseService.sendNotification(notificationVo, token);
     }
 }
