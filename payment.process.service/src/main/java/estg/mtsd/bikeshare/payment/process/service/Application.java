89f1d8b8dea7c8988821568ddf2d89e5a8b71668
Carlos <car.f.barbosa@gmail.com>
2021-01-18 22:58:22 +0000

    added imports

--- payment.process.service/src/main/java/estg/mtsd/bikeshare/payment/process/service/Application.java
+++ payment.process.service/src/main/java/estg/mtsd/bikeshare/payment/process/service/Application.java
@@ -2,6 +2,8 @@ package estg.mtsd.bikeshare.payment.process.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableEurekaClient
