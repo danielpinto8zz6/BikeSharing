40f3957a3a94f37ab8f00402a792eb5897f76414
Daniel Pinto <danielpinto8zz6@gmail.com>
2021-01-20 00:16:17 +0000

    fix notifications controller

--- notifications.service/src/main/java/estg/mtsd/bikeshare/notifications/service/controllers/NotificationController.java
+++ notifications.service/src/main/java/estg/mtsd/bikeshare/notifications/service/controllers/NotificationController.java
@@ -5,11 +5,13 @@ import com.google.firebase.messaging.FirebaseMessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import estg.mtsd.bikeshare.notifications.service.service.FirebaseMessagingService;
 import estg.mtsd.bikeshare.notifications.service.service.TokenService;
+import estg.mtsd.bikeshare.shared.library.utils.JwtUtils;
 import estg.mtsd.bikeshare.shared.library.vo.NotificationVo;
 
 @Controller
@@ -19,15 +21,19 @@ public class NotificationController {
 
     @Autowired
     TokenService tokenService;
-    
+
     public NotificationController(FirebaseMessagingService firebaseService) {
         this.firebaseService = firebaseService;
     }
 
     @RequestMapping("/send-notification")
     @ResponseBody
-    public String sendNotification(@RequestBody NotificationVo notificationVo) throws FirebaseMessagingException {
-        String token = tokenService.findById(notificationVo.getEmail());
+    public String sendNotification(@RequestBody NotificationVo notificationVo,
+            @RequestHeader("Authorization") String jwtToken) throws FirebaseMessagingException {
+
+        String email = JwtUtils.parseUserEmail(jwtToken);
+
+        String token = tokenService.findById(email);
 
         return firebaseService.sendNotification(notificationVo, token);
     }
