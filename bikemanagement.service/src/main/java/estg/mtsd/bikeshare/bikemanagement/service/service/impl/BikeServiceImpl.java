7f8b84fe4306d01be535e4a05f03e7e65c64185f
Daniel Pinto <danielpinto8zz6@gmail.com>
2021-01-21 19:24:56 +0000

    fixes

--- bikemanagement.service/src/main/java/estg/mtsd/bikeshare/bikemanagement/service/service/impl/BikeServiceImpl.java
+++ bikemanagement.service/src/main/java/estg/mtsd/bikeshare/bikemanagement/service/service/impl/BikeServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 
@@ -20,78 +19,71 @@ import estg.mtsd.bikeshare.bikemanagement.service.vo.BikeVo;
 @Service
 public class BikeServiceImpl implements BikeService {
 
-	@Autowired
-	BikeDao bikeDao;
+    @Autowired
+    BikeDao bikeDao;
 
-	@Override
-	@Transactional
-	public void save(BikeVo bikeVo) {	
-		Integer id = bikeVo.getId();
-		Boolean objectAlreadyExists=bikeDao.existsById(id);
-		if(!objectAlreadyExists) {
-			Bike bike = new Bike();
-			BeanUtils.copyProperties(bikeVo, bike);
-			bikeDao.save(bike);
-		}else {
-			throw new EntityExistsException();
-		}
-		
-	}
-	
-	@Override
-	@Transactional
-	public void update(BikeVo bikeVo) {
-		Integer id = bikeVo.getId();
-		Boolean objectExists=bikeDao.existsById(id);
-		if(objectExists) {
-			Bike bike = new Bike();
-			BeanUtils.copyProperties(bikeVo, bike);
-			bikeDao.save(bike);
-		}else {
-			throw new EntityNotFoundException();
-		}
-	}
+    @Override
+    @Transactional
+    public void save(BikeVo bikeVo) {
+        Bike bike = new Bike();
+        BeanUtils.copyProperties(bikeVo, bike);
+        bikeDao.save(bike);
+    }
 
-	@Override
-	@Transactional
-	public void delete(Integer id) {
-		Boolean objectExists=bikeDao.existsById(id);
-		if(objectExists) {
-			bikeDao.deleteById(id);
-		}else {
-			throw new EntityNotFoundException();
-		}
-	}
+    @Override
+    @Transactional
+    public void update(BikeVo bikeVo) {
+        Integer id = bikeVo.getId();
+        boolean objectExists = bikeDao.existsById(id);
+        if (objectExists) {
+            Bike bike = new Bike();
+            BeanUtils.copyProperties(bikeVo, bike);
+            bikeDao.save(bike);
+        } else {
+            throw new EntityNotFoundException();
+        }
+    }
 
-	@Override
-	@Transactional
-	public BikeVo get(Integer id) {
-		Optional<Bike> bikeOptional = bikeDao.findById(id);
-		BikeVo bikeVo=null;
-		if(bikeOptional.isPresent()) {
-			bikeVo = new BikeVo();
-			BeanUtils.copyProperties(bikeOptional.get(), bikeVo);	
-		}else {
-			throw new EntityNotFoundException();
-		}
-		
-		return bikeVo;
-	}
+    @Override
+    @Transactional
+    public void delete(Integer id) {
+        boolean objectExists = bikeDao.existsById(id);
+        if (objectExists) {
+            bikeDao.deleteById(id);
+        } else {
+            throw new EntityNotFoundException();
+        }
+    }
 
-	@Override
-	@Transactional
-	public List<BikeVo> getAll() {
-		List<Bike> bikeList = bikeDao.findAll();
-		List<BikeVo> bikeVoList = new ArrayList<>();
-		if (bikeList != null && !bikeList.isEmpty()) {
-			for (Bike bike : bikeList) {
-				BikeVo bikeVo = new BikeVo();
-				BeanUtils.copyProperties(bike, bikeVo);
-				bikeVoList.add(bikeVo);
-			}
-		}
-		return bikeVoList;
-	}
+    @Override
+    @Transactional
+    public BikeVo get(Integer id) {
+        Optional<Bike> bikeOptional = bikeDao.findById(id);
+        BikeVo bikeVo = null;
+        if (bikeOptional.isPresent()) {
+            bikeVo = new BikeVo();
+            BeanUtils.copyProperties(bikeOptional.get(), bikeVo);
+        } else {
+            throw new EntityNotFoundException();
+        }
+
+        return bikeVo;
+    }
+
+    @Override
+    @Transactional
+    public List<BikeVo> getAll() {
+        List<Bike> bikeList = bikeDao.findAll();
+        List<BikeVo> bikeVoList = new ArrayList<>();
+        if (!bikeList.isEmpty()) {
+            for (Bike bike : bikeList) {
+                BikeVo bikeVo = new BikeVo();
+                BeanUtils.copyProperties(bike, bikeVo);
+                bikeVoList.add(bikeVo);
+            }
+        }
+        return bikeVoList;
+    }
 
 }
 
--- bikemanagement.service/src/main/java/estg/mtsd/bikeshare/bikemanagement/service/vo/BikeVo.java
+++ bikemanagement.service/src/main/java/estg/mtsd/bikeshare/bikemanagement/service/vo/BikeVo.java
@@ -1,5 +1,8 @@
 package estg.mtsd.bikeshare.bikemanagement.service.vo;
 
+import lombok.Data;
+
+@Data
 public class BikeVo {
 
 	private Integer id ;
@@ -13,57 +16,5 @@ public class BikeVo {
 	private String brand ;
 
 	private String model ;
-
-	public BikeVo(){
-		super();
-	}
-
-	public Integer getId(){
-		return this.id;
-	}
-
-	public void setId(Integer id){
-		this.id = id;
-	}
-
-	public Integer getChargeLevel(){
-		return this.chargeLevel;
-	}
-
-	public void setChargeLevel(Integer chargeLevel){
-		this.chargeLevel = chargeLevel;
-	}
-
-	public Long getServiceHours(){
-		return this.serviceHours;
-	}
-
-	public void setServiceHours(Long serviceHours){
-		this.serviceHours = serviceHours;
-	}
-
-	public String getSerialNumber(){
-		return this.serialNumber;
-	}
-
-	public void setSerialNumber(String serialNumber){
-		this.serialNumber = serialNumber;
-	}
-
-	public String getBrand(){
-		return this.brand;
-	}
-
-	public void setBrand(String brand){
-		this.brand = brand;
-	}
-
-	public String getModel(){
-		return this.model;
-	}
-
-	public void setModel(String model){
-		this.model = model;
-	}
 
 }
