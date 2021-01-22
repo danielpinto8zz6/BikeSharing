d3b420654984dd27597bcca311695bb20902eaf4
Daniel Pinto <danielpinto8zz6@gmail.com>
2021-01-22 22:45:02 +0000

    update configs

--- dockmanagement.service/src/main/java/estg/mtsd/bikeshare/dockmanagement/service/listeners/DockClosedEventListener.java
+++ dockmanagement.service/src/main/java/estg/mtsd/bikeshare/dockmanagement/service/listeners/DockClosedEventListener.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
--- dockmanagement.service/src/main/java/estg/mtsd/bikeshare/dockmanagement/service/listeners/UnlockBikeEventListener.java
+++ dockmanagement.service/src/main/java/estg/mtsd/bikeshare/dockmanagement/service/listeners/UnlockBikeEventListener.java
@@ -1,18 +1,17 @@
 package estg.mtsd.bikeshare.dockmanagement.service.listeners;
 
+import estg.mtsd.bikeshare.dockmanagement.service.producers.OpenDockEventProducer;
 import estg.mtsd.bikeshare.dockmanagement.service.service.DockService;
 import estg.mtsd.bikeshare.shared.library.utils.JsonUtils;
 import estg.mtsd.bikeshare.shared.library.vo.DockVo;
+import estg.mtsd.bikeshare.shared.library.vo.OpenDockEvent;
 import estg.mtsd.bikeshare.shared.library.vo.UnlockBikeEvent;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UnlockBikeEventListener {
@@ -20,17 +19,11 @@ public class UnlockBikeEventListener {
     @Autowired
     DockService dockService;
 
-    @Value("${topic.unlock-bike.consumer")
-    private String topicName;
+    @Autowired
+    OpenDockEventProducer openDockEventProducer;
 
     @KafkaListener(topics = "${topic.unlock-bike.consumer", groupId = "dock-management")
     public void consume(ConsumerRecord<String, String> payload) {
-        log.info("Tópico: " + topicName);
-        log.info("key: " + payload.key());
-        log.info("Headers: " + payload.headers());
-        log.info("Partion: " + payload.partition());
-        log.info("Order: " + payload.value());
-
         UnlockBikeEvent event = JsonUtils.fromJson(payload.value(), UnlockBikeEvent.class);
 
         DockVo dock = dockService.get(event.getDockId());
@@ -39,8 +32,8 @@ public class UnlockBikeEventListener {
 
             dockService.update(dock);
 
-            // Unlock bike
-            //TODO: notify dummy
+            OpenDockEvent openDockEvent = new OpenDockEvent(dock.getBikeId(), dock.getId());
+            openDockEventProducer.send(openDockEvent);
         }
     }
 }
