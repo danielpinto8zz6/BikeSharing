248d08fc3bca223019d61857e013237b1f025cab
Carlos <car.f.barbosa@gmail.com>
2021-01-23 23:03:49 +0000

    bike validator deployment scripts

--- build.sh
+++ build.sh
@@ -27,35 +27,6 @@ echo 'building and pushing services/docker images...'
 ./travel.history.service/build-and-push-docker-image.sh
 ./zuul.server/build-and-push-docker-image.sh
 
-
-echo 'deploying to k8s...'
-
-./eureka.server/deploy.sh
-
-sleep 1m
-
-./zuul.server/deploy.sh
-./account.service/deploy.sh
-./auth.service/deploy.sh
-./bike.validator.service/deploy.sh
-./bikemanagement.service/deploy.sh
-./dock.service/deploy.sh
-./dockmanagement.service/deploy.sh
-./dummy.service/deploy.sh
-./feedback.service/deploy.sh
-./notifications.service/deploy.sh
-./payment.calculator.service/deploy.sh
-./payment.process.service/deploy.sh
-./payment.service/deploy.sh
-./payment.validator.service/deploy.sh
-./pgadmin.service/deploy.sh
-./rental.process.service/deploy.sh
-./rental.service/deploy.sh
-./token.manager/deploy.sh
-./travel.history.process.service/deploy.sh
-./travel.history.receiver.service/deploy.sh
-./travel.history.service/deploy.sh
-
 end=`date +%s`
 
 runtime=$((end-start))
--- deploy.sh
+++ deploy.sh
@@ -5,26 +5,30 @@ start=`date +%s`
 echo 'deploying to k8s...'
 
 ./eureka.server/deploy.sh
+
+sleep 1m
+
 ./zuul.server/deploy.sh
-./travel.history.process.service/deploy.sh
-./bikemanagement.service/deploy.sh
-./dockmanagement.service/deploy.sh
-./payment.calculator.service/deploy.sh
-./feedback.service/deploy.sh
-./payment.service/deploy.sh
 ./account.service/deploy.sh
 ./auth.service/deploy.sh
+./bike.validator.service/deploy.sh
+./bikemanagement.service/deploy.sh
 ./dock.service/deploy.sh
+./dockmanagement.service/deploy.sh
 ./dummy.service/deploy.sh
-./travel.history.receiver.service/deploy.sh
-./rental.service/deploy.sh
-./travel.history.service/deploy.sh
-./rental.process.service/deploy.sh
-./payment.process.service/deploy.sh
-./payment.validator.service/deploy.sh
+./feedback.service/deploy.sh
 ./notifications.service/deploy.sh
-./token.manager/deploy.sh
+./payment.calculator.service/deploy.sh
+./payment.process.service/deploy.sh
+./payment.service/deploy.sh
+./payment.validator.service/deploy.sh
 ./pgadmin.service/deploy.sh
+./rental.process.service/deploy.sh
+./rental.service/deploy.sh
+./token.manager/deploy.sh
+./travel.history.process.service/deploy.sh
+./travel.history.receiver.service/deploy.sh
+./travel.history.service/deploy.sh
 
 end=`date +%s`
 
