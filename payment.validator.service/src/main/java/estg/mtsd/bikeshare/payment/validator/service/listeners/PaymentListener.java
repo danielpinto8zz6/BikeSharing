e79517bad715aabe2a8066e03c22887e97671e19
Daniel Pinto <danielpinto8zz6@gmail.com>
2021-01-25 23:02:08 +0000

    payment simple validation

--- payment.validator.service/src/main/java/estg/mtsd/bikeshare/payment/validator/service/listeners/PaymentListener.java
+++ payment.validator.service/src/main/java/estg/mtsd/bikeshare/payment/validator/service/listeners/PaymentListener.java
@@ -1,14 +1,17 @@
 package estg.mtsd.bikeshare.payment.validator.service.listeners;
 
+import estg.mtsd.bikeshare.payment.validator.service.producers.NotificationProducer;
 import estg.mtsd.bikeshare.payment.validator.service.producers.PaymentEventProducer;
 import estg.mtsd.bikeshare.shared.library.utils.JsonUtils;
+import estg.mtsd.bikeshare.shared.library.vo.NotificationVo;
 import estg.mtsd.bikeshare.shared.library.vo.PaymentEvent;
 import estg.mtsd.bikeshare.shared.library.vo.PaymentVo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import rx.Notification;
+
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -19,11 +22,24 @@ public class PaymentListener {
     @Autowired
     PaymentEventProducer paymentEventProducer;
 
+    @Autowired
+    NotificationProducer notificationProducer;
 
     @KafkaListener(topics = "${topic.payment.consumer}", groupId = "payment-validator")
     public void consume(ConsumerRecord<String, String> payload) {
         PaymentVo payment = JsonUtils.fromJson(payload.value(), PaymentVo.class);
 
-        paymentEventProducer.send(new PaymentEvent(payment.getId(), PaymentEvent.PaymentStatus.PAYMENT_SUCCEED));
+        if (payment != null) {
+            if (payment.getName() != null) {
+                paymentEventProducer
+                        .send(new PaymentEvent(payment.getId(), PaymentEvent.PaymentStatus.PAYMENT_SUCCEED));
+            } else {
+                NotificationVo notificationVo = new NotificationVo();
+                notificationVo.setEmail(payment.getUserEmail());
+                notificationVo.setTitle("Payment failed!");
+
+                notificationProducer.send(notificationVo);
+            }
+        }
     }
 }
